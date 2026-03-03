f52998108de07a03953a988fc267f06a85574ab5
bitturana-ab <b2ra830@gmail.com>
2026-03-03 16:46:32 +0530

    starting type conversion

--- LEVEL-1/Hello.java
+++ LEVEL-1/Hello.java
@@ -2,5 +2,8 @@
 class Hello{
     public static void main(String[] arg){
         System.out.println("Hello AB");
+        int a = 1_00_00_000;
+        a++;
+        System.out.println(a);
     }
 }
