f159a0c7205700191140f4516d03068b4198bb62
bitturana-ab <b2ra830@gmail.com>
2026-03-03 17:06:16 +0530

    fixed: type conversion n casting

--- LEVEL-1/casting.java
+++ LEVEL-1/casting.java
@@ -2,15 +2,18 @@
 class casting {
 
     public static void main(String args[]) {
-        int a = 257; //257%256 = 1 
+        int a = 257; // 257%256 = 1 
         byte b = (byte) a;
-        byte c = 124; //max 127
+        System.out.println(b);
+        byte c = 124; // max 127
         int d = c; // byte to int no need to casting
+        System.out.println(d);
         float e = 5.6f;
-        int g = (int) e; //loss point value
+        int g = (int) e; // loss point value
+        System.out.println(g);
         int num = 6;
-        // int res = num++; // fetch the value and then increment //post - inrmt
-        int res = ++num; // inrement then fetch // pre - incmt
+        // int res = num++; // fetch the value and then increment //post - incrmt
+        int res = ++num; // inrement then fetch // pre - incrmt
         System.out.println(res);
 
         int x = 5;
